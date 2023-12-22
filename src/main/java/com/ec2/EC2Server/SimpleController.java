d1135e9d43ca20f3796779272db092f275a95d74
Sandesh Vaze <sandeshvaze@gmail.com>
2023-12-22 20:42:40 +0530

    updated

--- src/main/java/com/ec2/EC2Server/SimpleController.java
+++ src/main/java/com/ec2/EC2Server/SimpleController.java
@@ -2,8 +2,9 @@ package com.ec2.EC2Server;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class SimpleController {
 	
 	@GetMapping("/sayHello")
